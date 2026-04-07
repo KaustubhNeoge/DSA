4c2aba1e039f41a70a396ba8bab6409230a9ace3
Kaustubh <ksneoge@gmail.com>
2026-04-07 12:26:36 +0530

    Implement linked list creation and display functions

---
+++ Linked Lists/LLADT.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void Create(int A[], int n){
+    int i;
+    struct Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+    for(i = 1; i < n; i++){
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node *p){
+    cout << "Elements of the linked list are: ";
+    while(p != NULL){
+        cout << p->data << " ";
+        p = p-> next;
+    }
+}
+
+int main(){
+    int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Create(A, sizeof(A)/sizeof(A[0]));
+    Display(first);
+}
